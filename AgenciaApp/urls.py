d39c2d4635438c6b7d84a4253af31c7fe779eeec
Axel Ariel Godoy <axelariel78@gmail.com>
2024-09-05 00:12:06 -0300

    Actualizo el readme para mostrar el proyecto

--- AgenciaApp/urls.py
+++ AgenciaApp/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from .views import index, crear_auto, buscar_auto, sobre_nosotros, blog, editar_auto, eliminar_auto
+from .views import index, crear_auto, buscar_auto, sobre_nosotros, editar_auto, eliminar_auto
 
 urlpatterns = [
     path("", index, name="index"),
     path("crearauto/", crear_auto, name="crear_auto"),
     path("buscarauto/", buscar_auto, name="buscar_auto"),
     path("sobrenosotros/", sobre_nosotros, name="sobre_nosotros"),
-    path("blog/", blog, name="blog"),
     path("editarauto/<int:id>/", editar_auto, name="editar_auto"),
     path("eliminarauto/<int:id>/", eliminar_auto, name="eliminar_auto"),
 ]
